50b4be90f46573e0240900608c23944a58f9192f
Christian Neumann <cneumann@datenkarussell.de>
2012-09-29 20:38:20 +0200

    Remove old copyright information.

--- monsti/worker/doc.go
+++ monsti/worker/doc.go
@@ -1,7 +1,3 @@
-// Copyright 2012 Christian Neumann. All rights reserved.                                                                                          
-// Use of this source code is governed by a BSD license
-// that can be found in the COPYING file.
-
 /*
 Package worker implements operations to manage and communicate with a worker
 process that processes incoming requests for some node type.
