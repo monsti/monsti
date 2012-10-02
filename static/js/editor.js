fefc32049592ca674561693e34fdf65613021623
Christian Neumann <cneumann@datenkarussell.de>
2012-10-02 09:47:03 +0200

    Add markup help button and text for the javascript editor.

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,8 +1,19 @@
+// ToggleHelp shows or hides the markup help.
+ToggleHelp = function() {
+    help = $('#editor-help');
+    if(help.css('display') == "none") {
+        help.load('/static/html/editor_help.de.html');
+        help.slideDown();
+    } else {
+        help.slideUp();
+    }
+}
+
+// AddEditor transforms the given textarea to a fancy javascript markup editor.
 AddEditor = function(obj) {
     var $textarea = obj;
     $textarea.before("<div id=\"epiceditor\"></div>");
     $textarea.hide();
-
     var editor = new EpicEditor({
         basePath: '/static/epiceditor',
         theme: {
@@ -18,6 +29,11 @@ AddEditor = function(obj) {
       $textarea.val(file.content);
     });
     editor.load();
+    var help = '<div id="editor-help" style="display:none;">Loading...</div>'
+    $('#epiceditor').append(help);
+    var button = '<a id="btn-editor-help" href="#" title="Help"><img src="/static/img/icons/silk/help.png" alt="Help"/></a>';
+    $('#epiceditor').before(button);
+    $('#btn-editor-help').click(ToggleHelp);
 }
 
 $(document).ready(function () {
