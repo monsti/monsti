974ac3a0fcb528bfc368768b55d98489bb7a5568
Christian Neumann <cneumann@datenkarussell.de>
2013-06-27 14:17:06 +0200

    Add copyright notice.

--- monsti-image.go
+++ monsti-image.go
@@ -1,3 +1,24 @@
+// This file is part of Monsti, a web content management system.
+// Copyright 2012-2013 Christian Neumann
+//
+// Monsti is free software: you can redistribute it and/or modify it under the
+// terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
+//
+// Monsti is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Monsti.  If not, see <http://www.gnu.org/licenses/>.
+
+/*
+ Monsti is a simple and resource efficient CMS.
+
+ This package implements the image node type.
+*/
 package main
 
 import (
