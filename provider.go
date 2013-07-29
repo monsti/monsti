ed3489e490ed09abaec9b43ac1fa5b46d36f276a
Christian Neumann <cneumann@datenkarussell.de>
2013-07-29 18:20:35 +0200

    Remove old documentation.

--- provider.go
+++ provider.go
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with Monsti.  If not, see <http://www.gnu.org/licenses/>.
 
-//	Package provider provides utilities to construct service providers.
 package service
 
 import (
