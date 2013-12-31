7bdde65b6978cddd88605eb2fd5c1c19ca087eeb
Christian Neumann <cneumann@datenkarussell.de>
2013-12-31 16:03:27 +0100

    Add raw and resize options.

--- monsti-image.go
+++ monsti-image.go
@@ -27,6 +27,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/quirkey/magick"
 	"pkg.monsti.org/form"
 	"pkg.monsti.org/service"
 	"pkg.monsti.org/util"
@@ -53,9 +54,9 @@ func edit(req service.Request, res *service.Response, s *service.Session) {
 		"Title": form.Field{G("Title"), "", form.Required(G("Required.")), nil},
 		"Image": form.Field{G("Image"), "", nil, new(form.FileWidget)}})
 	switch req.Method {
-	case "GET":
+	case service.GetRequest:
 		data.Title = req.Node.Title
-	case "POST":
+	case service.PostRequest:
 		var imageData []byte
 		var err error
 		if len(req.Files["Image"]) == 1 {
@@ -83,7 +84,7 @@ func edit(req service.Request, res *service.Response, s *service.Session) {
 			form.AddError("Image", G("There was a problem with your image upload."))
 		}
 	default:
-		panic("Request method not supported: " + req.Method)
+		panic(fmt.Sprintf("Request method not supported: %v", req.Method))
 	}
 	rendered, err := renderer.Render("image/edit",
 		template.Context{"Form": form.RenderData()},
@@ -94,14 +95,68 @@ func edit(req service.Request, res *service.Response, s *service.Session) {
 	fmt.Fprint(res, rendered)
 }
 
+type size struct{ Width, Height int }
+
+func (s size) String() string {
+	return fmt.Sprintf("%vx%v", s.Width, s.Height)
+}
+
 func view(req service.Request, res *service.Response, s *service.Session) {
-	body, err := s.Data().GetNodeData(req.Site, req.Node.Path,
-		"image.data")
-	if err != nil {
-		panic("Could not get image data: " + err.Error())
+	sizeName := req.Query.Get("size")
+	var size size
+	var body []byte
+	var err error
+	if sizeName != "" {
+		err = s.Data().GetConfig(req.Site, "image", "sizes."+sizeName, &size)
+		if err != nil || size.Width == 0 {
+			logger.Printf("Could not find size %q for site %q: %v", sizeName, req.Site,
+				err)
+		} else {
+			sizePath := "image_" + size.String() + ".data"
+			body, err = s.Data().GetNodeData(req.Site, req.Node.Path, sizePath)
+			if err != nil || body == nil {
+				body, err = s.Data().GetNodeData(req.Site, req.Node.Path, "image.data")
+				if err != nil {
+					panic("Could not get image data: " + err.Error())
+				}
+				image, err := magick.NewFromBlob(body, "jpg")
+				if err != nil {
+					panic("Could not open image data with magick: " + err.Error())
+				}
+				defer image.Destroy()
+				err = image.Resize(size.String())
+				if err != nil {
+					panic("Could not resize image: " + err.Error())
+				}
+				body, err = image.ToBlob("jpg")
+				if err != nil {
+					panic("Could not dump image: " + err.Error())
+				}
+				if err := s.Data().WriteNodeData(req.Site, req.Node.Path,
+					sizePath, string(body)); err != nil {
+					panic("Could not write resized image data: " + err.Error())
+				}
+			}
+		}
 	}
-	res.Raw = true
-	res.Write(body)
+	if body == nil {
+		body, err = s.Data().GetNodeData(req.Site, req.Node.Path, "image.data")
+		if err != nil {
+			panic("Could not get image data: " + err.Error())
+		}
+	}
+	if req.Query.Get("raw") == "1" {
+		res.Raw = true
+		res.Write(body)
+	} else {
+		rendered, err := renderer.Render("image/view",
+			template.Context{"Image": req.Node},
+			req.Session.Locale, "")
+		if err != nil {
+			panic("Could not render template: " + err.Error())
+		}
+		fmt.Fprint(res, rendered)
+	}
 }
 
 func main() {
