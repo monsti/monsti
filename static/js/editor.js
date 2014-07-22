37b37c06430c9860e975ca98e25d960ee77e7606
Christian Neumann <cneumann@datenkarussell.de>
2014-07-22 13:03:51 +0200

    Add raw HTML editor toggle button.

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,20 +1,48 @@
-// AddEditor transforms the given textarea into an editable for Aloha.
-AddEditor = function(obj) {
-  // TODO This won't work for multiple textareas.
-  var $textarea = obj;
-  $textarea.hide();
-  var content = $textarea.val();
-  $textarea.before("<div class=\"editable\">" + content + "</div>");
-  editable = $('.editable')
-  Aloha.jQuery(editable).aloha();
-  $('button').click(function() {
-    editable.mahalo();
-    $textarea.val(editable.html());
-  });
-}
+(function() {
+  var disableEditor = function(textarea) {
+    var editable = textarea.previousSibling;
+    $(editable).mahalo();
+    textarea.value = editable.innerHTML;
+    editable.parentElement.removeChild(editable);
+    textarea.style.display = "block";
+    textarea.attributes.editorActive = false;
+  }
 
-$(document).ready(function () {
-  Aloha.ready( function() {
-    $('textarea.editor').each(function() { AddEditor($(this)); });
+  var enableEditor = function(textarea) {
+    textarea.style.display = "none";
+    var editable = document.createElement("div");
+    editable.classList.add("editable");
+    editable.innerHTML = textarea.value;
+    textarea.parentElement.insertBefore(editable, textarea);
+    Aloha.jQuery(editable).aloha();
+    textarea.attributes.editorActive = true;
+  }
+
+  // AddEditor transforms the given textarea into an editable for Aloha.
+  AddEditor = function(textarea) {
+    var button = document.createElement("button");
+    button.type = "button";
+    button.appendChild(document.createTextNode("Toggle raw HTML"));
+    button.addEventListener("click", function() {
+      if (textarea.attributes.editorActive == true) {
+        disableEditor(textarea);
+      } else {
+        enableEditor(textarea);
+      }
+    });
+    textarea.parentElement.insertBefore(button, textarea.nextSibling)
+    textarea.form.addEventListener("submit", function() {
+      if (textarea.attributes.editorActive == true) {
+        disableEditor(textarea);
+      }
+    });
+    enableEditor(textarea);
+  }
+
+  $(document).ready(function () {
+    Aloha.ready( function() {
+      $('textarea.editor').each(function() { AddEditor(this); });
+    });
   });
-});
+
+})();
