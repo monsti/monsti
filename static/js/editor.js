d225f6fadfed829b9f19e7ce08d8f43a64952f9c
Christian Neumann <cneumann@datenkarussell.de>
2014-07-22 11:39:01 +0200

    Reformat code.

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,20 +1,20 @@
 // AddEditor transforms the given textarea into an editable for Aloha.
 AddEditor = function(obj) {
-    // TODO This won't work for multiple textareas.
-    var $textarea = obj;
-    $textarea.hide();
-    var content = $textarea.val();
-    $textarea.before("<div class=\"editable\">" + content + "</div>");
-    editable = $('.editable')
-    Aloha.jQuery(editable).aloha();
-    $('button').click(function() {
-        editable.mahalo();
-        $textarea.val(editable.html());
-    });
+  // TODO This won't work for multiple textareas.
+  var $textarea = obj;
+  $textarea.hide();
+  var content = $textarea.val();
+  $textarea.before("<div class=\"editable\">" + content + "</div>");
+  editable = $('.editable')
+  Aloha.jQuery(editable).aloha();
+  $('button').click(function() {
+    editable.mahalo();
+    $textarea.val(editable.html());
+  });
 }
 
 $(document).ready(function () {
-    Aloha.ready( function() {
-        $('textarea.editor').each(function() { AddEditor($(this)); });
-    });
+  Aloha.ready( function() {
+    $('textarea.editor').each(function() { AddEditor($(this)); });
+  });
 });
