4de6b3943f79a7c3d84438beb9bf289176938a6f
Christian Neumann <cneumann@datenkarussell.de>
2015-02-09 15:15:53 +0100

    Use custom classes for WYSIWYG alignment / floats.

--- static/js/editor.js
+++ static/js/editor.js
@@ -27,6 +27,13 @@
       height: 300,
       file_browser_callback: monstiFileChooser,
       language : monsti.session.locale,
+      formats : {
+        alignleft : {selector : 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', classes : 'monsti--htmlarea-align-left'},
+        aligncenter : {selector : 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', classes : 'monsti--htmlarea-align-center'},
+        alignright : {selector : 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', classes : 'monsti--htmlarea-align-right'},
+        alignfull : {selector : 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', classes : 'monsti--htmlarea-align-full'},
+      },
+      content_css : ["/static/css/common.css","/site-static/css/site.css"],
     });
   });
 })();
