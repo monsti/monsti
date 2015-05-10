7a2cff39e800896bb1057ba7d063202d280df392
Christian Neumann <cneumann@datenkarussell.de>
2015-05-10 18:21:02 +0200

    Add CKEditor plugin One Time Widget.

--- static/js/editor.js
+++ static/js/editor.js
@@ -12,10 +12,11 @@ var monsti = monsti || {};
     defaultLanguage: monsti.session.locale,
     removePlugins: 'maximize,stylescombo',
     removeButtons: 'SpecialChar',
-    extraPlugins: 'autogrow,image2,showblocks',
+    extraPlugins: 'autogrow,image2,showblocks,onetimewidget',
     autoGrow_minHeight: 250,
     autoGrow_maxHeight: 600,
   };
+  CKEDITOR.plugins.addExternal('onetimewidget', '/static/lib/onetimewidget/', 'plugin.js' );
 })();
 
 // addCKEditor adds an CKEditor for the textarea with the given id.
